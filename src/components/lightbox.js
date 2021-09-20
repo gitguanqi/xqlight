cac0cad1e448ec9ceb43e261d23284ee058b2d3a
jcguanqi <jcguanqi@126.com>
2021-09-21 07:56:57 +0800

    remove vue-build

--- src/components/lightbox.js
+++
@@ -1,9 +0,0 @@
-import lightboxComponent from './lightbox.vue'
-
-const xqlightbox = {
-    install: function (Vue) {  
-        Vue.component('xqLightbox', lightboxComponent)
-    }
-}
-
-export default xqlightbox;
